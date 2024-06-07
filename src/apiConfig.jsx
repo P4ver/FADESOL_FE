e81be8660c00ef34afac9e47e4928b11aad6ddb8
P4ver <s.pales20@gmail.com>
2024-06-07 10:55:03 +0100

    tsf

--- src/apiConfig.jsx
+++ src/apiConfig.jsx
@@ -1,4 +1,4 @@
 // export const API_BASE_URL = 'https://fadesol-puoc.vercel.app';
 // export const API_BASE_URL = 'https://fadesol.vercel.app';
-export const API_BASE_URL = 'http://localhost:3000';
-// export const API_BASE_URL = 'https://fadesol.onrender.com';
+export const API_BASE_URL = 'https://fadesol.onrender.com';
+// export const API_BASE_URL = 'http://localhost:3000';
